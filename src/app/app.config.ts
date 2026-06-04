584bb8369488a5d476fd36cb6b1d21823410969b
Brayan Torres <tbrayan782@gmail.com>
2026-06-04 10:43:00 -0500

    styles: se agregan componentes y estilos de PrimeNg a la libreria de estilos, con paleta blue,surface viva y theme Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import { themePreset } from '@lib/styles';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: themePreset,
+        options: {
+          darkModeSelector: '.p-dark',
+        },
+      },
+    }),
   ]
 };
---
+++ src/lib/styles/theme.ts
@@ -0,0 +1,54 @@
+import { definePreset } from '@primeuix/themes';
+import material from '@primeuix/themes/material';
+
+export const themePreset = definePreset(material, {
+  semantic: {
+    primary: {
+      50: '{blue.50}',
+      100: '{blue.100}',
+      200: '{blue.200}',
+      300: '{blue.300}',
+      400: '{blue.400}',
+      500: '{blue.500}',
+      600: '{blue.600}',
+      700: '{blue.700}',
+      800: '{blue.800}',
+      900: '{blue.900}',
+      950: '{blue.950}',
+    },
+    colorScheme: {
+      light: {
+        surface: {
+          0: '#ffffff',
+          50: '{stone.50}',
+          100: '{stone.100}',
+          200: '{stone.200}',
+          300: '{stone.300}',
+          400: '{stone.400}',
+          500: '{stone.500}',
+          600: '{stone.600}',
+          700: '{stone.700}',
+          800: '{stone.800}',
+          900: '{stone.900}',
+          950: '{stone.950}',
+        },
+      },
+      dark: {
+        surface: {
+          0: '#ffffff',
+          50: '{stone.50}',
+          100: '{stone.100}',
+          200: '{stone.200}',
+          300: '{stone.300}',
+          400: '{stone.400}',
+          500: '{stone.500}',
+          600: '{stone.600}',
+          700: '{stone.700}',
+          800: '{stone.800}',
+          900: '{stone.900}',
+          950: '{stone.950}',
+        },
+      },
+    },
+  },
+});
