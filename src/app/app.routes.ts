2bdd347fe59c0e901192e21bac256b321d562a0a
jorge-torres-net <jorge.torres.arus@gmail.com>
2026-06-04 21:01:48 -0500

    Add routes for CompraArticuloComponent and redirect
    Configurar ruta dinamica para compra de articulo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CompraArticuloComponent } from './components/compra-articulo/compra-articulo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: 'compra/:id', 
+    component: CompraArticuloComponent 
+  },
+  { 
+    path: '', 
+    redirectTo: 'compra/101', 
+    pathMatch: 'full' 
+  }
+];
